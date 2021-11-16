2ecb2f16b5015eb24075330a91f016a50bf8bc14
EC2 Default User <ec2-user@ip-172-31-37-43.ec2.internal>
2021-11-16 02:44:20 +0000

    feat: SNS-9

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ApplicationRecord
   has_many :user_rooms
   has_many :chats
   has_many :rooms, through: :user_rooms
+  has_many :view_counts, dependent: :destroy
   
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
